3acceaf7d5e454aaff282bc548a0b2737389051b
Ori Lotan <ori_lotan@outlook.com>
2022-02-24 22:01:28 +0200

    Some fixed in the find_emplyees

--- find_employes.py
+++ find_employes.py
@@ -1,7 +1,7 @@
 from dataset import Project
 
 def find_employes_for_project(project, skills):
-    for i, skill in emulate(project.skills):
+    for i, skill in enumerate(project.skills):
         wanted_skill_level = skill.level
         if mentors[i] == True:
             wanted_skill_level-=1
@@ -20,11 +20,12 @@ def find_employes_for_project(project, skills):
             project.employees[i] = employee
 
             for employ_skill in employees.skills:
-                for j, skill_to_mentor in emulate(project.skills):
+                for j, skill_to_mentor in enumerate(project.skills):
                     if skill_to_mentor.name == employ_skill.name \
                         and employ_skill.level >= skill_to_mentor.level
                         project.mentors[j] = True
+            break
 
-            return True
+        return True
 
-        return False
+    return False
