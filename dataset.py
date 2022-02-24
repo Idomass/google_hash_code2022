10f0ea3da3534e8b3625460d78595f201df0f510
Ido Masubi <idomass@github.com>
2022-02-24 20:02:27 +0200

    Added all structs, renamed file

--- dataset.py
+++ dataset.py
@@ -1,10 +1,23 @@
 from typing import List
 from dataclasses import dataclass
 
+
+@dataclass
+class Skill:
+    name:   str
+    level:  int
+
+
+@dataclass
+class Employee:
+    name:   str
+    skills: List[Skill]
+
+
 @dataclass
 class Project:
     name:           str
     duration:       int
     score:          int
     best_before:    int
-    required_roles: List
+    required_roles: List[Skill]
