5f759384df2baef3eebdf37b8a05d724c6810403
Ori Lotan <ori_lotan@outlook.com>
2022-02-24 21:57:47 +0200

    Add support for bigger levels then wanted in the find emplyee function

--- find_employes.py
+++ find_employes.py
@@ -5,16 +5,26 @@ def find_employes_for_project(project, skills):
         wanted_skill_level = skill.level
         if mentors[i] == True:
             wanted_skill_level-=1
-
-        if len(skills[skill.name][wanted_skill_level]) == 0:
+        
+        if not (skill.name in skills.keys()):
             return False
 
-        employee = skills[skill.name][wanted_skill_level][0]
-        project.employees[i] = employee
+        for level in skills[skill.name].keys:
+            if level < wanted_skill_level:
+                continue
 
-        for employ_skill in employees.skills:
-            for j, skill_to_mentor in emulate(project.skills):
-                if skill_to_mentor.name == employ_skill.name \
-                    and employ_skill.level >= skill_to_mentor.level
-                    project.mentors[j] = True
-    return True
+            if len(skills[skill.name][level]) == 0:
+                continue
+
+            employee = skills[skill.name][level][0]
+            project.employees[i] = employee
+
+            for employ_skill in employees.skills:
+                for j, skill_to_mentor in emulate(project.skills):
+                    if skill_to_mentor.name == employ_skill.name \
+                        and employ_skill.level >= skill_to_mentor.level
+                        project.mentors[j] = True
+
+            return True
+
+        return False
