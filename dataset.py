7af1e70a0d690e72046dcc76089ae7642e7407f5
Ido Masubi <idomass@github.com>
2022-02-24 20:30:22 +0200

    Parsed input

--- dataset.py
+++ dataset.py
@@ -4,6 +4,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Skill:
+    def __init__(self, name: str, level: str):
+        self.name = name
+        self.level = int(level)
+
     name:   str
     level:  int
 
@@ -16,8 +20,15 @@ class Employee:
 
 @dataclass
 class Project:
+    def __init__(self, name: str, duration: str, score: str, best_before: str, skills: List[Skill]) -> None:
+        self.name           = name
+        self.duration       = int(duration)
+        self.score          = int(score)
+        self.best_before    = int(best_before)
+        self.skills         = skills
+
     name:           str
     duration:       int
     score:          int
     best_before:    int
-    required_roles: List[Skill]
+    skills:         List[Skill]
---
+++ main.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+
+import argparse
+
+from typing import List, Tuple
+from dataset import Skill, Employee, Project
+
+
+def parse_data(file) -> Tuple[List[Employee], List[Project]]:
+    """
+    Returns a list of avialable employees and projects
+    """
+
+    num_employee, num_projects = (int(x) for x in file.readline().strip('\n').split())
+
+    employees   = []
+    projects    = []
+    for _ in range(num_employee):
+        name, num_skills = file.readline().split()
+
+        skills = [Skill(*file.readline().split()) for _ in range(int(num_skills))]
+
+        employees.append(Employee(name, skills))
+
+    for _ in range(num_projects):
+        project_line = file.readline().split()
+        num_skills = int(project_line[4])
+
+        skills = [Skill(*file.readline().split()) for _ in range(int(num_skills))]
+
+        projects.append(Project(*project_line[:4], skills))
+
+    return employees, projects
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=argparse.FileType('r'))
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    print(parse_data(args.filename))
+
+
+
+
+
+if __name__ == '__main__':
+    main()
